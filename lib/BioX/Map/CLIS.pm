baa1147ec86f3efdbffba6557f803e8a95aa2c28
Yan Xueqing <yanxueqing001@berrygenomics.com>
2016-05-25 18:10:48 +0800

    Add clis module

---
+++ lib/BioX/Map/CLIS.pm
@@ -0,0 +1,21 @@
+package BioX::Map::CLIS;
+use Modern::Perl;
+use IO::All;
+use Moo;
+use Types::Standard qw/Int Str/;
+use MooX::Cmd;
+use MooX::Options prefer_commandline => 1;
+
+# VERSION
+# ABSTRACT: a mapping toolkit
+
+sub execute {
+  my ($self, $args_ref, $chain_ref) = @_;
+  my $pre_message = "\nWarning:\n  this is a apps collection, your can only execute it's sub_command or sub_sub_command. more detail can be obtain by --man paramter\n";
+  unless (@$args_ref) {
+    say $pre_message;
+    $self->options_usage;
+  }
+}
+
+1;
